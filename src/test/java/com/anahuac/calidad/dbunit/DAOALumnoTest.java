ca1d0999af5d11ee730dc78f8c44405a3cb0756b
Leo Garcia <gleo2519@gmail.com>
2021-11-29 11:12:20 -0600

    Update in localhost

--- src/test/java/com/anahuac/calidad/dbunit/DAOALumnoTest.java
+++ src/test/java/com/anahuac/calidad/dbunit/DAOALumnoTest.java
@@ -33,9 +33,9 @@ public class DAOALumnoTest extends DBTestCase {
 	public DAOALumnoTest() {
 		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_DRIVER_CLASS, "com.mysql.cj.jdbc.Driver");
 		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_CONNECTION_URL,
-				"jdbc:mysql://localhost:33060/pruebas_db");
+				"jdbc:mysql://127.0.0.1:33060/pruebas_db");
 		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_USERNAME, "root");
-		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_PASSWORD, "secret");
+		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_PASSWORD, "");
 	}
 
 	@Override
--- test/travis.sql
+++ test/travis.sql
@@ -2,8 +2,8 @@
 CREATE USER 'dev'@'localhost' IDENTIFIED BY 'dev';
 GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP ON *.* TO 'dev'@'localhost';
 
-CREATE DATABASE IF NOT EXISTS `prueba_db` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `prueba_db`;
+CREATE DATABASE IF NOT EXISTS `pruebas_db` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `pruebas_db`;
 
 CREATE TABLE IF NOT EXISTS `alumnos_tbl` (
   `id` int(11),
